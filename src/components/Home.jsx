27f21c687ceb536e0283944c80872576cdabfc8f
ibimina <96787210+ibimina@users.noreply.github.com>
2022-12-10 10:22:40 +0100

    docs: check in localstorage before update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
-import useFetch from '../hook/useFetch';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useFetch from "../hook/useFetch";
 
 export default function () {
-const url = "https://catfact.ninja/fact";
-  const { loading, error, catFact,  fetchData } = useFetch(url);
+  const url = "https://catfact.ninja/fact";
+  const { loading, error, catFact, fetchData } = useFetch(url);
+  const getStore = localStorage.getItem("catfacts");
+  const [room, setRoom] = useState([]);
+  useEffect(() => {
+    if (getStore) {
+      let updateLocalstorage = JSON.parse(getStore).concat(catFact);
+      localStorage.setItem("catfacts", JSON.stringify(updateLocalstorage));
+    } else {
+      setRoom(room.concat(catFact));
+      localStorage.setItem("catfacts", JSON.stringify(room));
+    }
+  }, [catFact]);
 
-const [room,setRoom]=useState([])
-useEffect(()=>{
-setRoom(room.concat(catFact))
-localStorage.setItem("catfacts", JSON.stringify(room));
-},[catFact])
-console.log(room)
   return (
     <>
       {catFact && (
         <div className="container">
           {loading && <>loading...</>}
           {error === true && <>error</>}
-          <Link to="/catfacts" className='link'>previous fact</Link>
+          <Link to="/catfacts" className="link">
+            previous fact
+          </Link>
           <p className="fact"> {catFact.fact}</p>
-          <button onClick={fetchData} className="getfact">get facts</button>
+          <button onClick={fetchData} className="getfact">
+            get facts
+          </button>
         </div>
       )}
     </>
